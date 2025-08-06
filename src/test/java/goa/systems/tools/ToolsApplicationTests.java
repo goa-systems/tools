09688ef11e6ea5e0d7b6051d313ce4d2ddaecbb1
AndreasGottardi <andreas@goa.systems>
2025-08-06 23:21:30 +0200

    Implementing test.

--- src/test/java/goa/systems/tools/ToolsApplicationTests.java
+++ src/test/java/goa/systems/tools/ToolsApplicationTests.java
@@ -1,13 +1,20 @@
 package goa.systems.tools;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ToolsApplicationTests {
 
+	@Value("${spring.application.name}")
+	private String appname;
+
 	@Test
 	void contextLoads() {
+		assertEquals("tools", appname);
 	}
 
 }
